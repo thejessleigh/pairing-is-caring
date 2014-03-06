8c8d7fb874b31b85be4cfef8f3fe48b54382f972
thejessleigh <j.l.unrein@gmail.com>
2014-03-06 15:24:55 -0600

    deleted unnecessary views leftover from skeleton. Took out nav bar that shouldn't be there. Added some placeholder text to the index view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ActiveRecord::Base
-  # validations should be taken care of through Socrates authorization
-  # validates :name, uniqueness: :true, presence: :true
-  # validates :email, uniqueness: :true, presence: :true
-  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  # validates :password, presence: true, length: { minimum: 8 }
-  # validates :password_confirmation, presence: true
-
-  # has_secure_password
-
   has_many :appointments, foreign_key :mentor_id
   has_many :mentees, through: :appointments
 
