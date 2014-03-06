208d89f7e35c627a51009a3e07c1ffe0f8ec5751
thejessleigh <j.l.unrein@gmail.com>
2014-03-06 16:26:30 -0600

    now connects to the Socrates API when ENVs are set. Fixed typos in associations.

--- app/controllers/schedules.rb
+++ app/controllers/schedules.rb
@@ -1,3 +1,4 @@
 get '/schedule' do
+  @user = User.find(session[:user_id])
   erb :schedule
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
-  has_many :appointments, foreign_key :mentor_id
+  has_many :appointments, foreign_key: :mentor_id
   has_many :mentees, through: :appointments
 
-  has_many :sessions, class_name: "Appointment", foreign_key :mentee_id
-  has many :mentors, through: :sessions
+  has_many :sessions, class_name: "Appointment", foreign_key: :mentee_id
+  has_many :mentors, through: :sessions
+
+
+  def self.create_or_find_user_from_oauth(oauth_hash)
+    user = self.find_by_email(oauth_hash.email)
+    unless user
+      user = self.create(name: oauth_hash.name,
+                         gravatar: oauth_hash.gravatar,
+                         email: oauth_hash.email)
+    end
+    user
+  end
+
 end
