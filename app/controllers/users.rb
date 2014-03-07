e90fd5a399956825dfd439849dad348ec596a13c
thejessleigh <j.l.unrein@gmail.com>
2014-03-06 22:46:45 -0600

    users can now see all of their appointments as both mentors and mentees.

---
+++ app/controllers/users.rb
@@ -0,0 +1,30 @@
+#=================================================Signin/Signout
+
+use OmniAuth::Builder do
+  provider :dbc, ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET']
+end
+
+get '/signin' do
+  redirect '/auth/dbc'
+end
+
+get '/auth/:provider/callback' do
+  @user =  User.create_or_find_user_from_oauth(request.env['omniauth.auth'].info)
+  session[:user_id] = @user.id
+  token = request.env['omniauth.auth'].credentials
+  session[:oauth_token] = token_as_hash(token)
+  redirect '/schedule'
+end
+
+get '/signout' do
+  session[:user_id] = nil
+end
+
+#===============================================Users
+
+get '/user' do
+  @user = User.find(session[:user_id])
+  @appointments = @user.appointments
+  @sessions = @user.sessions
+  erb :user
+end
